426eea1e4460a814eafe34190b45ac5a006e4a6b
John Long <jlcomp45@gmail.com>
2019-10-17 14:50:38 -0500

    completed favorite things exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 console.log("it works");
+const myInfo = document.querySelector("#domEntry");
+console.log("test", myInfo);
+
+document.querySelector("#displayEntry").addEventListener("click", event => {
+    // console.log("i clicked a button");
+    // takes info from fav thing box and holds it
+    const iWant = document.querySelector("#favoriteThing").value;
+    // takes info from address box and holds it
+    const address = document.querySelector("#address").value;
+// sends info to dom using innerHTML
+    myInfo.innerHTML = `
+    <section>
+        <h1>I can purchase ${iWant} at ${address}</h1>
+    </section>
+    `
+    console.log("mytest", myInfo.innerHTML);
+})
